69092cf32009e8c7ee4117fac27286995ca8026e
DoggyDowg <rickmcolella@gmail.com>
2025-02-17 20:09:11 +0800

    Fix build errors: Update ClientLayout props and Three.js imports

--- src/components/layouts/ClientLayout.tsx
+++ src/components/layouts/ClientLayout.tsx
@@ -3,12 +3,17 @@
 import { AssetLoadingProvider } from '@/contexts/AssetLoadingContext'
 import LoadingScreen from '@/components/shared/LoadingScreen'
 import Providers from '@/components/shared/Providers'
+import type { Property } from '@/types/property'
+
+interface ClientLayoutProps {
+  children: React.ReactNode
+  property?: Property
+}
 
 export default function ClientLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+  property
+}: ClientLayoutProps) {
   return (
     <AssetLoadingProvider>
       <LoadingScreen />
