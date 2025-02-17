775ee9400526eba5d394a4c8ef792eaac16be844
DoggyDowg <rickmcolella@gmail.com>
2025-02-17 21:30:57 +0800

    fix: make is_demo check more explicit and add debug logging

--- src/components/layouts/ClientLayout.tsx
+++ src/components/layouts/ClientLayout.tsx
@@ -1,24 +1,30 @@
 'use client'
 
+import { BrandColorInitializer } from '@/components/BrandColorInitializer'
+import { BrandFontInitializer } from '@/components/BrandFontInitializer'
 import { AssetLoadingProvider } from '@/contexts/AssetLoadingContext'
 import LoadingScreen from '@/components/shared/LoadingScreen'
-import Providers from '@/components/shared/Providers'
 import type { Property } from '@/types/property'
 
 interface ClientLayoutProps {
+  property: Property
   children: React.ReactNode
-  property?: Property
 }
 
-export default function ClientLayout({
-  children,
-  property
-}: ClientLayoutProps) {
+export function ClientLayout({ property, children }: ClientLayoutProps) {
+  console.log('[ClientLayout] Property data:', {
+    id: property.id,
+    is_demo: property.is_demo,
+    // Add other relevant properties here
+  });
+
   return (
     <AssetLoadingProvider>
-      <LoadingScreen property={property} />
+      {/* Only show loading screen for demo properties */}
+      {property.is_demo === true && <LoadingScreen />}
+      <BrandColorInitializer property={property} />
+      <BrandFontInitializer property={property} />
       {children}
-      <Providers />
     </AssetLoadingProvider>
   )
 }
