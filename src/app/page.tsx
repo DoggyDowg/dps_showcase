1135d7a2f04cdd4d13527e6bbb6eed7ccfa9aa57
DoggyDowg <rickmcolella@gmail.com>
2025-02-17 17:55:48 +0800

    fix: update root page to use production property ID

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 import { CuscoTemplate } from '@/templates/cusco/page'
 
 export default function Home() {
-  // Test property ID
-  const testPropertyId = '144e229e-34c9-4fa9-bb16-cfdc0dd0937a'
+  // Production property ID
+  const testPropertyId = '918bd332-c7a9-4541-ba06-68e4829206e4'
   
   return <CuscoTemplate propertyId={testPropertyId} />
 }
